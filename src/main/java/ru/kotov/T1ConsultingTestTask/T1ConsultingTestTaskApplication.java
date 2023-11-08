232df2b20fbb7a97ef5f25c4ad708c7a048abc8a
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-08 16:11:16 +0300

    Initial commit

---
+++ src/main/java/ru/kotov/T1ConsultingTestTask/T1ConsultingTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.kotov.T1ConsultingTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1ConsultingTestTaskApplication.class, args);
+	}
+
+}
