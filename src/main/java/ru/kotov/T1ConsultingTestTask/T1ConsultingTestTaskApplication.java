6cc212a10bce97bb53d532f6ba5f222c3d1f12c4
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-08 21:10:18 +0300

    added a description of the project

--- src/main/java/ru/kotov/T1ConsultingTestTask/T1ConsultingTestTaskApplication.java
+++ src/main/java/ru/kotov/T1ConsultingTestTask/T1ConsultingTestTaskApplication.java
@@ -1,9 +1,23 @@
 package ru.kotov.T1ConsultingTestTask;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Test Task",
+				version="1.0.0",
+				description = "This project allows you to get the frequency of characters in a string",
+				contact = @Contact(
+						name = "Dmitry Kotov",
+						email = "dskotov003@gmail.com"
+				)
+		)
+)
 public class T1ConsultingTestTaskApplication {
 
 	public static void main(String[] args) {
