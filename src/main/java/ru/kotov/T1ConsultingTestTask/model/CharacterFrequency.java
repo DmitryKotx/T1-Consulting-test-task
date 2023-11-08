e250460b90d88a8e3b0189cca6feb9cad16d2b87
DimaKotovProgrammist <dskotov003@gmail.com>
2023-11-08 18:57:52 +0300

    added the character frequency model class

---
+++ src/main/java/ru/kotov/T1ConsultingTestTask/model/CharacterFrequency.java
@@ -0,0 +1,14 @@
+package ru.kotov.T1ConsultingTestTask.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CharacterFrequency {
+
+    private char character;
+
+    private int frequency;
+}
